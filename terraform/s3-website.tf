9330b3c969816f712f0d171c6deae206745314a3
Paul Bygrave <paulrpbygrave@gmail.com>
2021-08-09 21:25:49 +0100

    retry

--- terraform/s3-website.tf
+++ terraform/s3-website.tf
@@ -1,6 +1,18 @@
 resource "aws_s3_bucket" "website" {
   bucket = var.domain
   acl    = "public-read"
+  policy = <<EOF
+    {
+      "Version":"2008-10-17",
+      "Statement":[{
+        "Sid":"AllowPublicRead",
+        "Effect":"Allow",
+        "Principal": {"AWS": "*"},
+        "Action":["s3:GetObject"],
+        "Resource":["arn:aws:s3:::${var.domain}/*"]
+      }]
+    }
+  EOF
 
   website {
     index_document = "index.html"
@@ -16,18 +28,7 @@ resource "aws_s3_bucket" "website" {
     enabled = true
   }
 
-  policy = <<EOF
-    {
-      "Version":"2008-10-17",
-      "Statement":[{
-        "Sid":"AllowPublicRead",
-        "Effect":"Allow",
-        "Principal": {"AWS": "*"},
-        "Action":["s3:GetObject"],
-        "Resource":["arn:aws:s3:::${var.domain}/*"]
-      }]
-    }
-  EOF
+
 
   tags = {
     Name    = "${var.domain}-website-bucket"
