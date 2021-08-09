0c329f3f4091bd192257602ee1397b7cbf366621
Paul Bygrave <paulrpbygrave@gmail.com>
2021-08-09 21:00:06 +0100

    s3 ref

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -3,7 +3,7 @@ resource "aws_route53_record" "domain" {
    zone_id = data.aws_route53_zone.domain.zone_id
    type = "A"
    alias {
-     name = aws_s3_bucket.website.bucket_domain.name
+     name = aws_s3_bucket.website.bucket_domain_name
      zone_id = aws_s3_bucket.website.hosted_zone_id
      evaluate_target_health = true
    }
