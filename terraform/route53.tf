eeb8ecfd8c9a3024a49fa5c581fa604fd5be1259
Paul Bygrave <paulrpbygrave@gmail.com>
2021-08-09 21:37:21 +0100

    remove r53

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -1,11 +1,11 @@
-resource "aws_route53_record" "root_domain" {
-  zone_id = data.aws_route53_zone.domain.zone_id
-  name    = var.domain
-  type    = "A"
+# resource "aws_route53_record" "root_domain" {
+#   zone_id = data.aws_route53_zone.domain.zone_id
+#   name    = var.domain
+#   type    = "A"
 
-  alias {
-    name                   = aws_cloudfront_distribution.cdn.domain_name
-    zone_id                = aws_cloudfront_distribution.cdn.hosted_zone_id
-    evaluate_target_health = false
-  }
-}
+#   alias {
+#     name                   = aws_cloudfront_distribution.cdn.domain_name
+#     zone_id                = aws_cloudfront_distribution.cdn.hosted_zone_id
+#     evaluate_target_health = false
+#   }
+# }
