56ac9f1616bb2359959a99c89a4c7dcfa5981911
Paul Bygrave <paulrpbygrave@gmail.com>
2021-08-09 20:01:40 +0100

    remove the redirects

--- terraform/s3-website.tf
+++ terraform/s3-website.tf
@@ -5,17 +5,6 @@ resource "aws_s3_bucket" "website" {
   website {
     index_document = "index.html"
     error_document = "error.html"
-
-    routing_rules = <<EOF
-[{
-    "Condition": {
-        "KeyPrefixEquals": "docs/"
-    },
-    "Redirect": {
-        "ReplaceKeyPrefixWith": "documents/"
-    }
-}]
-EOF
   }
 
   logging {
