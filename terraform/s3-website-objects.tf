4ec1a0b2fa396c40f22666083ae5a6cab72550e7
Paul Bygrave <paulrpbygrave@gmail.com>
2021-08-09 19:58:07 +0100

    updates

--- terraform/s3-website-objects.tf
+++ terraform/s3-website-objects.tf
@@ -1,10 +1,15 @@
 resource "aws_s3_bucket_object" "index" {
-  bucket = aws_s3_bucket.website.name
+  bucket = aws_s3_bucket.website.id
   key    = "index.html"
   source = "${path.module}/files/index.html"
 
-  # The filemd5() function is available in Terraform 0.11.12 and later
-  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
-  # etag = "${md5(file("path/to/file"))}"
   etag = filemd5("${path.module}/files/index.html")
+}
+
+resource "aws_s3_bucket_object" "error" {
+  bucket = aws_s3_bucket.website.id
+  key    = "error.html"
+  source = "${path.module}/files/error.html"
+
+  etag = filemd5("${path.module}/files/error.html")
 }
