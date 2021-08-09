dad9c1e06bd3e7fffd5fe3b289a210f684428e5d
Paul Bygrave <paulrpbygrave@gmail.com>
2021-08-09 22:47:30 +0100

    roll with it

--- terraform/s3-website-objects.tf
+++ terraform/s3-website-objects.tf
@@ -1,5 +1,6 @@
 resource "aws_s3_bucket_object" "index" {
   bucket       = aws_s3_bucket.website_bucket.id
+  acl          = "public-read"
   key          = "index.html"
   source       = "${path.module}/files/index.html"
   content_type = "text/html"
@@ -8,6 +9,7 @@ resource "aws_s3_bucket_object" "index" {
 
 resource "aws_s3_bucket_object" "error" {
   bucket       = aws_s3_bucket.website_bucket.id
+  acl          = "public-read"
   key          = "error.html"
   source       = "${path.module}/files/error.html"
   content_type = "text/html"
@@ -15,8 +17,10 @@ resource "aws_s3_bucket_object" "error" {
 }
 
 resource "aws_s3_bucket_object" "logo" {
-  bucket = aws_s3_bucket.website_bucket.id
-  key    = "checkout2.png"
-  source = "${path.module}/files/checkout2.png"
-  etag   = filemd5("${path.module}/files/checkout2.png")
+  bucket       = aws_s3_bucket.website_bucket.id
+  acl          = "public-read"
+  key          = "images/checkout2.png"
+  source       = "${path.module}/images/checkout2.png"
+  content_type = "image/jpeg"
+  etag         = filemd5("${path.module}/images/checkout2.png")
 }
