b5572b0e7e27c70e60daadd59f0e35a1dc113d34
Paul Bygrave <paulrpbygrave@gmail.com>
2021-08-09 19:43:52 +0100

    fix the bucket policy

--- terraform/s3-website.tf
+++ terraform/s3-website.tf
@@ -34,7 +34,23 @@ EOF
   }
 }
 
-resource "aws_s3_bucket" "log_bucket" {
-  bucket = "${local.name}-log-bucket"
-  acl    = "log-delivery-write"
+resource "aws_s3_bucket_policy" "website" {
+  bucket = aws_s3_bucket.website.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Id      = "WebsiteBucketPolicy"
+    Statement = [
+      {
+        Sid       = "IPAllow"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource = [
+          aws_s3_bucket.website.arn,
+          "${aws_s3_bucket.website.arn}/*",
+        ]
+      }
+    ]
+  })
 }
